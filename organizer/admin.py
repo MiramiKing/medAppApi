22d0d8ac23f25bb48d055910ab72e1ae4fcfd212
akmubi <akmubi@yandex.ru>
2021-05-20 18:46:25 +0500

    Исправление регистрации моделей

--- organizer/admin.py
+++ organizer/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
-from django.db.models import Model
-import inspect
-from organizer import models
+from organizer.models import Record
 
-for name, obj in inspect.getmembers(models):
-    if inspect.isclass(obj) and issubclass(obj, Model) and not obj._meta.abstract:
-        admin.site.register(obj)
+admin.site.register(Record)
